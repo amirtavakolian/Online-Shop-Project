57b3bb446269a2410ba837c018a8603a65db71ac
Amir Tavakolian <rezatva@gmail.com>
2024-02-17 13:27:23 +0330

    add panel module

---
+++ Modules/Panel/App/Http/Controllers/PanelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Panel\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PanelController extends Controller
+{
+    public function index()
+    {
+        return view('panel::index');
+    }
+}
