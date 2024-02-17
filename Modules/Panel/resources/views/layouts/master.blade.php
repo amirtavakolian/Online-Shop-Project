ec4ba9cd608e7f1b0e9a0791d36383a76938f350
Amir Tavakolian <rezatva@gmail.com>
2024-02-17 16:30:30 +0330

    complete panel module - add front template

--- Modules/Panel/resources/views/layouts/master.blade.php
+++ Modules/Panel/resources/views/layouts/master.blade.php
@@ -1,29 +1,390 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Panel Module - {{ config('app.name', 'Laravel') }}</title>
-
-    <meta name="description" content="{{ $description ?? '' }}">
-    <meta name="keywords" content="{{ $keywords ?? '' }}">
-    <meta name="author" content="{{ $author ?? '' }}">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    {{-- Vite CSS --}}
-    {{-- {{ module_vite('build-panel', 'resources/assets/sass/app.scss') }} --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Webprog.ir - Dashboard</title>
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('modules/panel/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('modules/panel/css/sb-admin-2.min.css') }}" rel="stylesheet" type="text/css">
 </head>
 
-<body>
-    @yield('content')
+<body id="page-top">
 
-    {{-- Vite JS --}}
-    {{-- {{ module_vite('build-panel', 'resources/assets/js/app.js') }} --}}
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion pr-0" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">WebProg.ir</div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="index.html">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span> داشبورد </span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            لورم
+        </div>
+
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+               aria-expanded="true"
+               aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i>
+                <span> صفحات </span>
+            </a>
+            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header"> صفحات ورود : </h6>
+                    <a class="collapse-item" href="login.html"> ورود </a>
+                    <a class="collapse-item" href="register.html"> عضویت </a>
+                    <a class="collapse-item" href="forgot-password.html"> فراموشی رمز عبور </a>
+                    <div class="collapse-divider"></div>
+                    <h6 class="collapse-header"> صفحات دیگر : </h6>
+                    <a class="collapse-item" href="404.html">404 Page</a>
+                    <a class="collapse-item" href="#">Blank Page</a>
+                </div>
+            </div>
+        </li>
+
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span> نمودار ها </span></a>
+        </li>
+
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="fas fa-fw fa-table"></i>
+                <span> جداول </span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <!-- Sidebar Toggle (Topbar) -->
+                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle">
+                    <i class="fa fa-bars" style="margin-top: 4px;"></i>
+                </button>
+
+                <!-- Topbar Search -->
+                <form class="d-none d-sm-inline-block form-inline ml-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    <div class="input-group">
+                        <input type="text" class="form-control bg-light border-0 small order-2" placeholder="جستجو ..."
+                               aria-label="Search" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="button">
+                                <i class="fas fa-search fa-sm"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+                <!-- Topbar Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                    <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                    <li class="nav-item dropdown no-arrow d-sm-none">
+                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                           data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-search fa-fw"></i>
+                        </a>
+                        <!-- Dropdown - Messages -->
+                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                             aria-labelledby="searchDropdown">
+                            <form class="form-inline mr-auto w-100 navbar-search">
+                                <div class="input-group">
+                                    <input type="text" class="form-control bg-light border-0 small order-2"
+                                           placeholder="جستجو ..."
+                                           aria-label="Search" aria-describedby="basic-addon2">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button">
+                                            <i class="fas fa-search fa-sm"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </li>
+
+                    <!-- Nav Item - Alerts -->
+                    <li class="nav-item dropdown no-arrow mx-1">
+                        <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                           data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-bell fa-fw"></i>
+                            <!-- Counter - Alerts -->
+                            <span class="badge badge-danger badge-counter">3+</span>
+                        </a>
+                        <!-- Dropdown - Alerts -->
+                        <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in text-right"
+                             aria-labelledby="alertsDropdown">
+                            <h6 class="dropdown-header">
+                                لورم ایپسوم
+                            </h6>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+                                <div>
+                                    <div class="small text-gray-500"> مهر 12, 1399</div>
+                                    <span class="font-weight-bold"> متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                      گرافیک است </span>
+                                </div>
+
+                                <div class="mr-3">
+                                    <div class="icon-circle bg-primary">
+                                        <i class="fas fa-file-alt text-white"></i>
+                                    </div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+
+                                <div>
+                                    <div class="small text-gray-500"> مهر 12, 1399</div>
+                                    متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                </div>
+
+                                <div class="mr-3">
+                                    <div class="icon-circle bg-success">
+                                        <i class="fas fa-donate text-white"></i>
+                                    </div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+
+                                <div>
+                                    <div class="small text-gray-500"> مهر 12, 1399</div>
+                                    متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                                </div>
+
+                                <div class="mr-3">
+                                    <div class="icon-circle bg-warning">
+                                        <i class="fas fa-exclamation-triangle text-white"></i>
+                                    </div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item text-center small text-gray-500" href="#"> مشاهده تمام </a>
+                        </div>
+                    </li>
+
+                    <!-- Nav Item - Messages -->
+                    <li class="nav-item dropdown no-arrow mx-1">
+                        <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
+                           data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-envelope fa-fw"></i>
+                            <!-- Counter - Messages -->
+                            <span class="badge badge-danger badge-counter">7</span>
+                        </a>
+                        <!-- Dropdown - Messages -->
+                        <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in text-right"
+                             aria-labelledby="messagesDropdown">
+                            <h6 class="dropdown-header">
+                                کامنت ها
+                            </h6>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">
+
+                                <div class="font-weight-bold">
+                                    <div class="text-truncate">
+                                        متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                                    </div>
+                                    <div class="small text-gray-500"> لورم ایپسوم</div>
+                                </div>
+
+                                <div class="dropdown-list-image mr-3">
+                                    <img class="rounded-circle" src="#" alt="">
+                                    <div class="status-indicator bg-success"></div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">
+
+                                <div>
+                                    <div class="text-truncate">
+                                        متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                                    </div>
+                                    <div class="small text-gray-500"> لورم ایپسوم</div>
+                                </div>
+
+                                <div class="dropdown-list-image mr-3">
+                                    <img class="rounded-circle" src="#" alt="">
+                                    <div class="status-indicator"></div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">
+
+                                <div>
+                                    <div class="text-truncate">
+                                        متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                                    </div>
+                                    <div class="small text-gray-500"> لورم ایپسوم</div>
+                                </div>
+
+                                <div class="dropdown-list-image mr-3">
+                                    <img class="rounded-circle" src="#" alt="">
+                                    <div class="status-indicator bg-warning"></div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">
+
+                                <div>
+                                    <div class="text-truncate">
+                                        متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+                                    </div>
+                                    <div class="small text-gray-500"> لورم ایپسوم</div>
+                                </div>
+
+                                <div class="dropdown-list-image mr-3">
+                                    <img class="rounded-circle" src="#" alt="">
+                                    <div class="status-indicator bg-success"></div>
+                                </div>
+
+                            </a>
+                            <a class="dropdown-item text-center small text-gray-500" href="#"> مشاهده تمام کامنت ها </a>
+                        </div>
+                    </li>
+
+                    <div class="topbar-divider d-none d-sm-block"></div>
+
+                    <!-- Nav Item - User Information -->
+                    <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            <span class="ml-2 d-none d-lg-inline text-gray-600 small"> علی شیخ </span>
+                            <img class="img-profile rounded-circle" src="{{ asset('modules/panel/img/user.jpg') }}">
+                        </a>
+                        <!-- Dropdown - User Information -->
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in text-right"
+                             aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user fa-sm fa-fw ml-2 text-gray-400"></i>
+                                پروفایل
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-cogs fa-sm fa-fw ml-2 text-gray-400"></i>
+                                تنظیمات
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw ml-2 text-gray-400"></i>
+                                خروج
+                            </a>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800"> @yield('page-title') </h1>
+                </div>
+                @yield('content')
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; WebProg.ir 1399</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"> لورم ایپسوم متن ساختگی </h5>
+                <button class="close ml-0" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body"> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک
+                است.
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-primary" href="login.html"> خروج </a>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal"> لغو</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{ asset('modules/panel/js/jquery.min.js') }}"></script>
+<script src="{{ asset('modules/panel/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('modules/panel/js/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('modules/panel/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('modules/panel/js/Chart.min.js') }}"></script>
+<script src="{{ asset('modules/panel/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('modules/panel/js/demo/chart-pie-demo.js') }}"></script>
 </body>
+
+</html>
