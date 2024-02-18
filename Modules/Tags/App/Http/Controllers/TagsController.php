dc0e76476c67fac44b837a99ebef80dfce06b235
Amir Tavakolian <rezatva@gmail.com>
2024-02-18 15:37:37 +0330

    init tag module

---
+++ Modules/Tags/App/Http/Controllers/TagsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Tags\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TagsController extends Controller
+{
+    
+}
---
+++ Modules/Tags/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('tags::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('tags.name') !!}</p>
+@endsection
