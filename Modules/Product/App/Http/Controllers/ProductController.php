03f7d2d6d82acc17add19a4a417afc298c110abe
Amir Tavakolian <rezatva@gmail.com>
2024-02-28 14:31:49 +0330

    init product module

---
+++ Modules/Product/App/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Product\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+
+
+    public function create()
+    {
+
+    }
+}
