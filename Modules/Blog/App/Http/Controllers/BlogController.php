6c736b02c5f06cba9bb33ac79ca959767a14d7ae
Amir Tavakolian <rezatva@gmail.com>
2024-04-23 17:07:38 +0330

    init blog module

---
+++ Modules/Blog/App/Http/Controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Blog\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BlogController extends Controller
+{
+
+}
---
+++ Modules/Blog/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Blog\App\Http\Controllers\BlogController;
+
+
