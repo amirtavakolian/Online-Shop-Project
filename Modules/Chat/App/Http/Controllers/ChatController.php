97cc93f6a53c398e446bc4156f4cbdc4db2b52b9
Amir Tavakolian <rezatva@gmail.com>
2024-04-30 14:57:10 +0330

    add Chat module

---
+++ Modules/Chat/App/Http/Controllers/ChatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Chat\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ChatController extends Controller
+{
+
+}
---
+++ Modules/Chat/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Chat\App\Http\Controllers\ChatController;
+
