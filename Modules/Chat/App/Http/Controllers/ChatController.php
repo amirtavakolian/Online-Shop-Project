d23cf8af32bf724bc930d63bf5e6bb28657ed68a
Amir Tavakolian <rezatva@gmail.com>
2024-05-04 11:31:30 +0330

    add chat system

--- Modules/Chat/App/Http/Controllers/ChatController.php
+++ Modules/Chat/App/Http/Controllers/ChatController.php
@@ -3,11 +3,32 @@
 namespace Modules\Chat\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redis;
+use Modules\Auth\App\Models\User;
+use Modules\Chat\App\Events\PrivateChatEvent;
+use Modules\Chat\App\Events\SendMessageEvent;
+use Modules\Chat\App\Events\ShowOnlineUsersEvent;
+use Modules\Chat\App\Events\UserHasJoinedTheChatEvent;
+use Psy\Util\Json;
 
 class ChatController extends Controller
 {
 
+    public function index()
+    {
+        UserHasJoinedTheChatEvent::dispatch(193);
+        return view('chat::index');
+    }
+
+    public function send(Request $request)
+    {
+        SendMessageEvent::dispatch($request->all(), auth()->user());
+    }
+
+    public function privateChatView(Request $request,User $user)
+    {
+        PrivateChatEvent::dispatch($request->input(['message']), $user->email);
+        return view('chat::private-chat');
+    }
 }
--- Modules/Chat/routes/web.php
+++ Modules/Chat/routes/web.php
@@ -3,3 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Chat\App\Http\Controllers\ChatController;
 
+Route::group(['prefix' => 'chat', 'middleware' => 'auth'], function () {
+
+    Route::get('/', [ChatController::class, 'index']);
+    Route::post('/send', [ChatController::class, 'send'])->name('send');
+    Route::post('/online-users/refresh', [ChatController::class, 'refreshOnlineUsers'])->name('online-users.refresh');
+
+    Route::get('/private-chat/{user:email}/view', [ChatController::class, 'privateChatView'])
+        ->name('private-chat-view');
+
+});
