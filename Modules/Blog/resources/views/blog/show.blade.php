5839665dcd56596ded51073bdafabe91acd6e114
Amir Tavakolian <rezatva@gmail.com>
2024-05-12 15:22:59 +0330

    add 'check if comment is disabled' statement to show post view

--- Modules/Blog/resources/views/blog/show.blade.php
+++ Modules/Blog/resources/views/blog/show.blade.php
@@ -25,30 +25,36 @@
                         {!!  $post->content !!}
                     </p>
                 </div>
-                <div class="comments_form">
-                    <h5>دیدگاه شما </h5>
-                    @guest()
-                        <div class="alert alert-danger">
-                            <p>جهت ثبت کامنت لطفا <a href="{{ route('login.index') }}">وارد شوید</a> </p>
-                        </div>
-                    @else
-                        <form>
-                            <div class="form-row">
-                                <div class="col-md-6">
-                                    <input type="text" value="{{ auth()->user()->name }}" class="form-control" placeholder="نام شما">
-                                </div>
-                                <div class="col-md-12">
-                                    <textarea class="form-control" placeholder="نظر شما ..."></textarea>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="text-left">
-                                        <button class="btn btn-primary">ثبت نظر</button>
+                @if($post->disable_comment == 1)
+                    <div class="alert alert-danger">
+                        <p>ثبت کامنت غیر فعال است</p>
+                    </div>
+                @else
+                    <div class="comments_form">
+                        <h5>دیدگاه شما </h5>
+                        @guest()
+                            <div class="alert alert-danger">
+                                <p>جهت ثبت کامنت لطفا <a href="{{ route('login.index') }}">وارد شوید</a> </p>
+                            </div>
+                        @else
+                            <form>
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <input type="text" value="{{ auth()->user()->name }}" class="form-control" placeholder="نام شما">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <textarea class="form-control" placeholder="نظر شما ..."></textarea>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="text-left">
+                                            <button class="btn btn-primary">ثبت نظر</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </form>
-                    @endguest
-                </div>
+                            </form>
+                        @endguest
+                    </div>
+                @endif
             </div>
         </div>
     </div>
