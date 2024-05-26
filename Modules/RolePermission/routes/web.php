4a49405d4fe98eb91c60eab312c1a49ca03f3150
Amir Tavakolian <rezatva@gmail.com>
2024-05-26 18:46:54 +0330

    add 'give users role' feature

--- Modules/RolePermission/routes/web.php
+++ Modules/RolePermission/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\RolePermission\App\Http\Controllers\PermissionController;
 use Modules\RolePermission\App\Http\Controllers\RoleController;
+use Modules\RolePermission\App\Http\Controllers\UserRoleController;
 
 Route::group(['prefix' => '/panel'], function () {
     Route::resource('permissions', PermissionController::class)->except('show');
@@ -10,4 +11,8 @@ Route::group(['prefix' => '/panel'], function () {
 
 Route::group(['prefix' => '/panel'], function () {
     Route::resource('roles', RoleController::class)->except('show');
+});
+
+Route::group(['prefix' => '/panel'], function () {
+    Route::resource('user-roles', UserRoleController::class)->except('show');
 });
