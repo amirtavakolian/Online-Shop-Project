f0a5acbb0c371314a4149796f641e6d8c22994c9
Amir Tavakolian <rezatva@gmail.com>
2024-05-28 11:18:50 +0330

    init ticket module

---
+++ Modules/Ticket/App/Http/Controllers/TicketController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Ticket\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TicketController extends Controller
+{
+
+}
---
+++ Modules/Ticket/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Ticket\App\Http\Controllers\TicketController;
+
