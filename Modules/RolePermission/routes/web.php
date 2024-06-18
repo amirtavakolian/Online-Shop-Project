0c72722f92170cb2b2f0b64a77688623259be467
Amir Tavakolian <rezatva@gmail.com>
2024-06-18 14:31:07 +0330

    refactor routes

--- Modules/RolePermission/routes/web.php
+++ Modules/RolePermission/routes/web.php
@@ -1,22 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Modules\RolePermission\App\Http\Controllers\PermissionController;
 use Modules\RolePermission\App\Http\Controllers\RoleController;
 use Modules\RolePermission\App\Http\Controllers\UserRoleController;
 
-Route::group(['middleware' => 'auth'], function () {
-
-    Route::group(['prefix' => '/panel'], function () {
-        Route::resource('permissions', PermissionController::class)->except('show');
-    });
-
-    Route::group(['prefix' => '/panel'], function () {
-        Route::resource('roles', RoleController::class)->except('show');
-    });
-
-    Route::group(['prefix' => '/panel'], function () {
-        Route::resource('user-roles', UserRoleController::class)->except('show');
-    });
+Route::group(['prefix' => '/panel', 'middleware' => 'auth'], function () {
+    Route::resource('permissions', PermissionController::class)->except('show');
+    Route::resource('roles', RoleController::class)->except('show');
+    Route::resource('user-roles', UserRoleController::class)->except('show');
 });
