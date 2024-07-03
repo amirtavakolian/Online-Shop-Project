a8942a571390c2b597306ccdd2b80f667ef9a6ea
Amir Tavakolian <rezatva@gmail.com>
2024-07-03 18:03:43 +0330

    remove home module

--- Modules/Home/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('home::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('home.name') !!}</p>
-@endsection
--- Modules/Home/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Home\App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
